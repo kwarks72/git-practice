5202e9b91bb98dbe8cb3e0565465aab3884c50ed
곽수연 <kwarks72@gmail.com>
2026-04-13 11:17:12 +0900

    string

---
+++ string.py
@@ -0,0 +1,15 @@
+sun="친구가 \"햇님이 좋아\"라고 말했다"
+print(sun)
+
+sunny="친구가 \"햇살이 좋은날\"이라고 말했다"
+print(sunny)
+
+fruits="바나나\n사과\n오렌지"
+print(fruits)
+
+txt7="""큰 따옴표와 작은 따옴포를 모두 포함한 문장"""
+print(txt7)
+
+print("="*50)
+print("my program")
+print("="*50)
