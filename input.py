368977d67be3e729cde25acb7e20ddd5f9823549
곽수연 <kwarks72@gmail.com>
2026-04-13 11:17:58 +0900

    string을 배워보자

--- input.py
+++ input.py
@@ -1,7 +1,18 @@
-a=10
-b=20
-average=(a+b)/2
-print(average)
+# #국어 80  #영어 75  수학=55 평균 점수를 구해라
 
-f=100
-c=(f-32)*(5/9)
+# korean =80
+# english= 75
+# mathmetics = 55
+# sum= korean+english+mathmetics
+# average= sum/3
+# print("홍길동의 평균점수는" , average)
+
+#자연수 13이 홀수인지 짝수인지 판별해라 
+#2로 나누어서 0이면 짝수 2로 안 나뉘면 홀수
+thirteen=13
+residue= thirteen %2
+print("13의 나머지는", residue)
+if residue == 1:
+    print("나머지가 1이므로 홀수다")
+else:
+    print("나머지가 2이므로 짝수다")
